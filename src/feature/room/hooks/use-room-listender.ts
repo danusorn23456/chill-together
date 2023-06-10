7ed88c4d4290336fa226acb004b0a2c3600b80cf
danusorn23456 <danusorn23456@gmail.com>
2023-06-10 19:26:25 +0700

    make code more readable by separate of each handle function

--- src/feature/room/hooks/use-room-listender.ts
+++ src/feature/room/hooks/use-room-listender.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { UserRecord, supabase } from "~/service/supabase";
 import { Channel } from "../type";
 import { useRecoilValue, useSetRecoilState } from "recoil";
@@ -7,6 +7,12 @@ import { roomIdState, roomState, UsersInRoom } from "../store";
 import { userState } from "~/feature/auth";
 import { useRoomId } from ".";
 import { randomBetween } from "~/feature/common/utils";
+import {
+  REALTIME_SUBSCRIBE_STATES,
+  RealtimeChannel,
+  RealtimePresenceJoinPayload,
+  RealtimePresenceLeavePayload,
+} from "@supabase/supabase-js";
 
 function useRoomListener() {
   const room_id = useRoomId();
@@ -15,11 +21,94 @@ function useRoomListener() {
   const setUsersInRoom = useSetRecoilState(usersInRoomState);
   const setRoomId = useSetRecoilState(roomIdState);
 
+  const usersChannel = useRef<RealtimeChannel>();
+
+  function handleSync() {
+    let usersInRoom = Object.values(usersChannel!.current!.presenceState()).map(
+      (i) => i[0]
+    ) as unknown as UsersInRoom;
+
+    console.log(
+      `%c online users ${usersInRoom.length}`,
+      "background:royalblue;color:white;padding:4px;"
+    );
+
+    usersInRoom = usersInRoom.sort((a, b) => {
+      if (a.is_owner && !b.is_owner) {
+        return -1; // a should come before b
+      } else if (!a.is_owner && b.is_owner) {
+        return 1; // a should come after b
+      }
+
+      const dateA = new Date(a.online_at);
+      const dateB = new Date(b.online_at);
+
+      return dateA.getTime() - dateB.getTime();
+    });
+
+    setUsersInRoom(usersInRoom);
+  }
+
+  function handleJoin({
+    newPresences,
+  }: RealtimePresenceJoinPayload<UserRecord>) {
+    const user = newPresences[0];
+    console.log(
+      `%c ${user.username} in the room`,
+      "background:royalblue;color:white;padding:4px;"
+    );
+  }
+
+  function handleLeave({
+    leftPresences,
+  }: RealtimePresenceLeavePayload<UserRecord>) {
+    const user = leftPresences[0];
+    console.log(
+      `%c ${user.username} left the room`,
+      "background:royalblue;color:white;padding:4px;"
+    );
+  }
+
+  async function handleSubScribe(status: `${REALTIME_SUBSCRIBE_STATES}`) {
+    const channel = usersChannel.current;
+
+    if (status === "SUBSCRIBED") {
+      const generateXPosition = randomBetween(5, -5);
+      const generateYPosition = randomBetween(1, -3);
+
+      await channel!.track({
+        is_owner: room?.created_by === user!.id,
+        id: user!.id,
+        username: user!.username,
+        avatar_url: user!.avatar_url,
+        online_at: new Date().toISOString(),
+        coordinates: [generateXPosition, generateYPosition, 0],
+      });
+
+      channel!.send({
+        type: "boardcast",
+        event: "a",
+        payload: "awd",
+      });
+
+      console.log(
+        `%c success to subscribe room`,
+        "background:green;color:white;padding:4px;"
+      );
+    }
+  }
+
+  useEffect(() => {
+    if (room_id) {
+      setRoomId(room_id);
+    }
+  }, [room_id]);
+
   useEffect(
     function onlineUsersSubscribe() {
       if (!user || !room) return;
 
-      const usersChannel = supabase.channel(Channel.ONLINE_USERS + room_id, {
+      usersChannel.current = supabase.channel(Channel.ONLINE_USERS + room_id, {
         config: {
           presence: {
             key: user!.id,
@@ -27,88 +116,23 @@ function useRoomListener() {
         },
       });
 
-      usersChannel.on("presence", { event: "sync" }, () => {
-        let usersInRoom = Object.values(usersChannel.presenceState()).map(
-          (i) => i[0]
-        ) as unknown as UsersInRoom;
+      const channel = usersChannel.current;
 
-        console.log(
-          `%c online users ${usersInRoom.length}`,
-          "background:royalblue;color:white;padding:4px;"
-        );
+      channel.on("presence", { event: "sync" }, handleSync);
 
-        usersInRoom = usersInRoom.sort((a, b) => {
-          if (a.is_owner && !b.is_owner) {
-            return -1; // a should come before b
-          } else if (!a.is_owner && b.is_owner) {
-            return 1; // a should come after b
-          }
+      channel.on("presence", { event: "join" }, handleJoin);
 
-          const dateA = new Date(a.online_at);
-          const dateB = new Date(b.online_at);
+      channel.on("presence", { event: "leave" }, handleLeave);
 
-          return dateA.getTime() - dateB.getTime();
-        });
-
-        setUsersInRoom(usersInRoom);
-      });
-
-      usersChannel.on("presence", { event: "join" }, ({ newPresences }) => {
-        const user = newPresences[0] as unknown as UserRecord;
-        console.log(
-          `%c ${user.username} in the room`,
-          "background:royalblue;color:white;padding:4px;"
-        );
-      });
-
-      usersChannel.on("presence", { event: "leave" }, ({ leftPresences }) => {
-        const user = leftPresences[0] as unknown as UserRecord;
-        console.log(
-          `%c ${user.username} left the room`,
-          "background:royalblue;color:white;padding:4px;"
-        );
-      });
-
-      usersChannel.subscribe(async (status) => {
-        if (status === "SUBSCRIBED") {
-          const generateXPosition = randomBetween(5, -5);
-          const generateYPosition = randomBetween(1, -3);
-
-          await usersChannel.track({
-            is_owner: room?.created_by === user.id,
-            id: user.id,
-            username: user.username,
-            avatar_url: user.avatar_url,
-            online_at: new Date().toISOString(),
-            coordinates: [generateXPosition, generateYPosition, 0],
-          });
-
-          usersChannel.send({
-            type: "boardcast",
-            event: "a",
-            payload: "awd",
-          });
-
-          console.log(
-            `%c success to subscribe room`,
-            "background:green;color:white;padding:4px;"
-          );
-        }
-      });
+      channel.subscribe(handleSubScribe);
 
       return () => {
-        usersChannel.unsubscribe();
-        usersChannel.untrack();
+        channel.unsubscribe();
+        channel.untrack();
       };
     },
     [room, user]
   );
-
-  useEffect(() => {
-    if (room_id) {
-      setRoomId(room_id);
-    }
-  }, [room_id]);
 
   return null;
 }
