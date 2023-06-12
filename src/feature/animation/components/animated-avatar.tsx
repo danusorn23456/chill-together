c6da90647c7582734102b20e47229445c0a68526
danusorn23456 <danusorn23456@gmail.com>
2023-06-12 08:55:09 +0700

    animate scale-up when hover

--- src/feature/animation/components/animated-avatar.tsx
+++ src/feature/animation/components/animated-avatar.tsx
@@ -1,4 +1,4 @@
-import { useTexture } from "@react-three/drei";
+import { Text, useTexture } from "@react-three/drei";
 import { UserInRoom } from "~/feature/room";
 import { motion } from "framer-motion-3d";
 
@@ -9,22 +9,36 @@ function AnimatedAvatar(user: UserInRoom) {
   const texture = useTexture(user.avatar_url);
 
   return (
-    <motion.sprite
-      position={[x, y, z]}
-      animate={{ y: y + 0.1 }}
-      transition={{ repeat: Infinity, duration: 0.5, repeatType: "reverse" }}
-    >
-      <motion.spriteMaterial
-        rotation={-0.05}
-        animate={
-          {
-            rotation: 0.05,
-          } as any
-        }
-        transition={{ repeat: Infinity, duration: 1, repeatType: "reverse" }}
-        map={texture}
-      />
-    </motion.sprite>
+    <>
+      <motion.sprite
+        whileHover={{
+          scale: 1.2,
+        }}
+        position={[x, y, z]}
+        animate={{
+          y: y + 0.1,
+          transition: {
+            repeat: Infinity,
+            duration: 0.5,
+            repeatType: "reverse",
+          },
+        }}
+      >
+        <motion.spriteMaterial
+          rotation={-0.05}
+          animate={
+            {
+              rotation: 0.05,
+            } as any
+          }
+          transition={{ repeat: Infinity, duration: 1, repeatType: "reverse" }}
+          map={texture}
+        />
+      </motion.sprite>
+      <Text color="white" scale={0.2} position={[x, y - 0.6, z]}>
+        {user.username}
+      </Text>
+    </>
   );
 }
 
