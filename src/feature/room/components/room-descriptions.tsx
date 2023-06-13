91d839ec294b3815319e34ed6bf216dfc31a9413
danusorn23456 <danusorn23456@gmail.com>
2023-06-13 21:42:35 +0700

    sort lobby base on owner

--- src/feature/room/components/room-descriptions.tsx
+++ src/feature/room/components/room-descriptions.tsx
@@ -1,18 +1,24 @@
 import { MouseEventHandler } from "react";
 import { GetRoomByIdResponseSuccess } from "../services";
-import { ArrowLeftOnRectangleIcon } from "@heroicons/react/20/solid";
 
 export interface RoomDescreiptionsProps {
+  isMe?: boolean;
   room: Required<GetRoomByIdResponseSuccess>;
   onClick: MouseEventHandler;
 }
 
 function RoomDescriptions({
+  isMe,
   room,
   onClick = () => {},
 }: RoomDescreiptionsProps) {
   return (
-    <div className="flex justify-between sm:items-center flex-col sm:flex-row p-4 rounded w-full max-w-xl bg-gray-900 sm:space-x-4 relative">
+    <div
+      className={[
+        isMe ? "border-dashed" : "",
+        "border border-gray-700 flex justify-between sm:items-center flex-col sm:flex-row p-4 rounded w-full max-w-xl bg-gray-900 sm:space-x-4 relative",
+      ].join(" ")}
+    >
       <div className="w-12 h-12 absolute opacity-0 sm:opacity-100 sm:static sm:visible bg-white rounded-full p-2 flex-shrink-0">
         <img
           className="w-full h-full"
@@ -36,7 +42,7 @@ function RoomDescriptions({
           onClick={onClick}
           className="p-1 px-4 text-sm duration-200 text-pink-400 rounded border border-pink-400 hover:border-pink-200 hover:bg-pink-500 hover:text-pink-100"
         >
-          join
+          join {isMe ? "my room" : ""}
         </button>
       </div>
     </div>
--- src/pages/user/lobby.tsx
+++ src/pages/user/lobby.tsx
@@ -1,13 +1,23 @@
+import { useRecoilValue } from "recoil";
+import { userState } from "~/feature/auth";
 import { RoomDescriptions, useRooms } from "~/feature/room";
 
 export interface LobbyProps {}
 
 function Lobby({}: LobbyProps) {
   const { rooms, join } = useRooms();
+  const user = useRecoilValue(userState);
+  const otherRooms = rooms?.filter((room) => room.owner.id !== user?.id);
+  let myRoom = rooms?.find((room) => room.owner.id === user?.id);
 
   return (
     <div className="pt-14 bg-gray-950 h-full w-full flex flex-col justify-start items-center space-y-4 p-4">
-      {rooms?.map((room) => (
+      {myRoom ? (
+        <RoomDescriptions isMe room={myRoom} onClick={() => join(myRoom!.id)} />
+      ) : (
+        <></>
+      )}
+      {otherRooms?.map((room) => (
         <RoomDescriptions
           key={room.id}
           room={room}
